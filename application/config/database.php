d05e21468a580c5d64f67b090290d14d32c44dbe
jethro <jethro@kayweb.com.au>
2017-07-16 14:22:11 +0800

    db - pdo

--- application/config/database.php
+++ application/config/database.php
@@ -94,3 +94,25 @@ $db['default'] = array(
 	'failover' => array(),
 	'save_queries' => TRUE
 );
+
+$db['pdo'] = array(
+    'dsn'   => 'mysql:host=localhost;dbname=icrime',
+    //'hostname' => 'localhost',
+    'username' => 'root',
+    'password' => '',
+    'database' => 'icrime',
+    'dbdriver' => 'pdo',
+    'dbprefix' => '',
+    'pconnect' => FALSE,
+    'db_debug' => (ENVIRONMENT !== 'production'),
+    'cache_on' => FALSE,
+    'cachedir' => '',
+    'char_set' => 'utf8',
+    'dbcollat' => 'utf8_general_ci',
+    'swap_pre' => '',
+    'encrypt' => FALSE,
+    'compress' => FALSE,
+    'stricton' => FALSE,
+    'failover' => array(),
+    'save_queries' => TRUE
+);
