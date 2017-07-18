ae5553c5c471b2069c67e82c7e41465be97a382a
jethro <jethro@localhost.localdomain>
2017-07-18 19:35:17 +0800

    loader changes

--- js/script.js
+++ js/script.js
@@ -8,6 +8,19 @@ $(document).ready(function()
     $('.add-app-user').click(function(){
         $("#add-app-user-modal").modal();
     });
+    
+    $('#app_user_form').submit(function(){
+        return false;
+    });
+
 });
 
+var loading = function()
+{
+    $('.loader').slideDown();
+};
 
+var dismissLoading = function()
+{
+    $('.loader').slideUp();
+};
