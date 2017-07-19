92c35986d75869ed24a8465923931e65ca555c61
jethro <jethro@localhost.localdomain>
2017-07-19 19:44:08 +0800

    repaired Admin controller

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -3,172 +3,157 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Admin extends CI_Controller {
 
-	/**
-	 * Index Page for this controller.
-	 *
-	 * Maps to the following URL
-	 * 		http://example.com/index.php/welcome
-	 *	- or -
-	 * 		http://example.com/index.php/welcome/index
-	 *	- or -
-	 * Since this controller is set as the default controller in
-	 * config/routes.php, it's displayed at http://example.com/
-	 *
-	 * So any other public methods not prefixed with an underscore will
-	 * map to /index.php/welcome/<method_name>
-	 * @see https://codeigniter.com/user_guide/general/urls.html
-	 */
-        public function __construct() {
-            parent::__construct();
-            $this->load->library('session'); 
-            $this->load->model('AdminModel','model');
-            $this->load->helper('url');
-        }
-	public function index()
-	{
-            $this->load->view('Admin/AdminHeader');
-            $this->load->view('Admin/Admin');
-            $this->load->view('Admin/AdminFooter');
-	}
-        
-        public function AppUsers()
+    public function __construct() {
+        parent::__construct();
+        $this->load->library('session'); 
+        $this->load->model('AdminModel','model');
+        $this->load->helper('url');
+    }
+    public function index()
+    {
+        $this->load->view('Admin/AdminHeader');
+        $this->load->view('Admin/Admin');
+        $this->load->view('Admin/AdminFooter');
+    }
+
+    public function AppUsers()
+    {
+        $data = array();
+        $data['list'] = '';
+        $stmt = $this->model->GetAppUsers();
+        foreach($stmt->result() as $row)
         {
-            $data = array();
-            $data['list'] = '';
-            $stmt = $this->model->GetAppUsers();
-            foreach($stmt->result() as $row)
-            {
-                $data['list'] .= $this->load->view('Admin/AppUsersList',$row,TRUE);
-            }
-                    
-            $this->load->view('Admin/AdminHeader');
-            $this->load->view('Admin/AppUsers',$data);
-            $this->load->view('Admin/AdminFooter');
+            $data['list'] .= $this->load->view('Admin/AppUsersList',$row,TRUE);
         }
-        
-        public function AppUsersAjax()
+
+        $this->load->view('Admin/AdminHeader');
+        $this->load->view('Admin/AppUsers',$data);
+        $this->load->view('Admin/AdminFooter');
+    }
+
+    public function AppUsersAjax()
+    {
+        $data = array();
+        $data['list'] = '';
+        $stmt = $this->model->GetAppUsers();
+        foreach($stmt->result() as $row)
         {
-            $data = array();
-            $data['list'] = '';
-            $stmt = $this->model->GetAppUsers();
-            foreach($stmt->result() as $row)
-            {
-                $data['list'] .= $this->load->view('Admin/AppUsersList',$row,TRUE);
-            }
-            
-            $data['success'] = TRUE;
-            echo json_encode($data);
-            exit;
+            $data['list'] .= $this->load->view('Admin/AppUsersList',$row,TRUE);
         }
-        
-        public function AddAppUser()
+
+        $data['success'] = TRUE;
+        echo json_encode($data);
+        exit;
+    }
+
+    public function AddAppUser()
+    {
+        $json_data = array();
+        $json_data['success'] = $this->model->AddAppUser($_POST);;
+        echo json_encode($json_data);
+        exit;
+    }
+
+    public function UpdateAppUser()
+    {
+        $json_data = array();
+        $json_data['success'] = $this->model->UpdateAppUser($_POST);;
+        echo json_encode($json_data);
+        exit;
+    }
+
+    public function DeleteAppUser()
+    {
+        $json_data = array();
+        $json_data['success'] = $this->model->DeleteAppUser($_POST['id']);
+        echo json_encode($json_data);
+        exit;
+    }
+
+    public function GetAppUserById()
+    {
+        $json_data = array();
+        $json_data['info'] = $this->model->GetAppUserById($_POST['id']);
+        $json_data['success'] = TRUE;
+        echo json_encode($json_data);
+        exit;
+    }
+
+    public function Stations()
+    {
+        $data = array();
+        $data['list'] = '';
+        $stmt = $this->model->GetStations();
+        foreach($stmt->result() as $row)
         {
-            $json_data = array();
-            $json_data['success'] = $this->model->AddAppUser($_POST);;
-            echo json_encode($json_data);
-            exit;
+            $data['list'] .= $this->load->view('Admin/Stations/StationsList',$row,TRUE);
         }
-        
-        public function UpdateAppUser()
+
+        $data['district_list'] = $this->BuildDistrictList();
+
+        $this->load->view('Admin/AdminHeader');
+        $this->load->view('Admin/Stations/Stations',$data);
+        $this->load->view('Admin/AdminFooter');
+    }
+
+    public function BuildDistrictList()
+    {
+        $list = '';
+        $stmt = $this->model->GetDistricts();
+        foreach($stmt->result() as $row)
         {
-            $json_data = array();
-            $json_data['success'] = $this->model->UpdateAppUser($_POST);;
-            echo json_encode($json_data);
-            exit;
+            $list .= $this->load->view('Admin/Stations/DistrictList',$row,TRUE);
         }
-        
-        public function DeleteAppUser()
+
+        return $list;
+    }
+
+    public function AddStation()
+    {
+        $json_data = array();
+        $json_data['success'] = $this->model->AddStation($_POST);
+        echo json_encode($json_data);
+        exit;
+    }
+
+    public function StationsAjax()
+    {
+        $json_data = array();
+        $json_data['list'] = '';
+        $stmt = $this->model->GetStations();
+        foreach($stmt->result() as $row)
         {
-            $json_data = array();
-            $json_data['success'] = $this->model->DeleteAppUser($_POST['id']);
-            echo json_encode($json_data);
-            exit;
+            $json_data['list'] .= $this->load->view('Admin/Stations/StationsList',$row,TRUE);
         }
-        
-        public function GetAppUserById()
-        {
-            $json_data = array();
-            $json_data['info'] = $this->model->GetAppUserById($_POST['id']);
-            $json_data['success'] = TRUE;
-            echo json_encode($json_data);
-            exit;
-        }
-        
-        public function Stations()
-        {
-            $data = array();
-            $data['list'] = '';
-            $stmt = $this->model->GetStations();
-            foreach($stmt->result() as $row)
-            {
-                $data['list'] .= $this->load->view('Admin/Stations/StationsList',$row,TRUE);
-            }
-            
-            $data['district_list'] = $this->BuildDistrictList();
-                    
-            $this->load->view('Admin/AdminHeader');
-            $this->load->view('Admin/Stations/Stations',$data);
-            $this->load->view('Admin/AdminFooter');
-        }
-        
-        public function BuildDistrictList()
-        {
-            $list = '';
-            $stmt = $this->model->GetDistricts();
-            foreach($stmt->result() as $row)
-            {
-                $list .= $this->load->view('Admin/Stations/DistrictList',$row,TRUE);
-            }
-            
-            return $list;
-        }
-        
-        public function AddStation()
-        {
-            $json_data = array();
-            $json_data['success'] = $this->model->AddStation($_POST);
-            echo json_encode($json_data);
-            exit;
-        }
-        
-        public function StationsAjax()
-        {
-            $json_data = array();
-            $json_data['list'] = '';
-            $stmt = $this->model->GetStations();
-            foreach($stmt->result() as $row)
-            {
-                $json_data['list'] .= $this->load->view('Admin/Stations/StationsList',$row,TRUE);
-            }
-            $json_data['success'] = TRUE;
-            echo json_encode($json_data);
-            exit;
-        }
-        
-        public function GetStationById()
-        {
-            $json_data = array();
-            $json_data['info'] = $this->model->GetStationById($_POST['id']);
-            $json_data['info']['edit_id'] = $json_data['info']['id'];
-            $json_data['info']['submit'] = 'Update';
-            $json_data['success'] = TRUE;
-            echo json_encode($json_data);
-            exit;
-        }
-        
-        public function UpdateStation()
-        {
-            $json_data = array();
-            $json_data['success'] = $this->model->UpdateStation($_POST);;
-            echo json_encode($json_data);
-            exit;
-        }
-        
-        public function DeleteStation()
-        {
-            $json_data = array();
-            $json_data['success'] = $this->model->DeleteStation($_POST['id']);
-            echo json_encode($json_data);
-            exit;
-        }
+        $json_data['success'] = TRUE;
+        echo json_encode($json_data);
+        exit;
+    }
+
+    public function GetStationById()
+    {
+        $json_data = array();
+        $json_data['info'] = $this->model->GetStationById($_POST['id']);
+        $json_data['info']['edit_id'] = $json_data['info']['id'];
+        $json_data['info']['submit'] = 'Update';
+        $json_data['success'] = TRUE;
+        echo json_encode($json_data);
+        exit;
+    }
+
+    public function UpdateStation()
+    {
+        $json_data = array();
+        $json_data['success'] = $this->model->UpdateStation($_POST);;
+        echo json_encode($json_data);
+        exit;
+    }
+
+    public function DeleteStation()
+    {
+        $json_data = array();
+        $json_data['success'] = $this->model->DeleteStation($_POST['id']);
+        echo json_encode($json_data);
+        exit;
+    }
 }
