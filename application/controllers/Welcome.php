d11071f01267135d613725a1ab26aa4d001937ae
jethro <jethro@kayweb.com.au>
2017-08-26 02:30:01 +0800

    crime analysis by week - done

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -61,6 +61,24 @@ class Welcome extends CI_Controller {
         exit;
     }
     
+    public function GetCrimeAnalysisByWeek()
+    {
+        $week_data = explode('-', $_POST['week']);
+        $year = $week_data[0];
+        $week = $week_data[1];
+        $week = ltrim($week, 'W');
+
+        $date_from = date('Y-m-d', strtotime($year."W".str_pad($week,2,"0",STR_PAD_LEFT)));
+        $date_to =  date('Y-m-d', strtotime("+6 days", strtotime($date_from)));
+        
+        $json_data = array();
+        $content = $this->model->GetCrimeAnalysisByDateRange($date_from,$date_to);
+        $json_data['content'] = $content->result();
+        $json_data['success'] = TRUE;
+        echo json_encode($json_data);
+        exit;
+    }
+    
     public function GetCrimeAnalysisByDay()
     {
         $day = $_POST['day'];
--- js/frontscript.js
+++ js/frontscript.js
@@ -132,6 +132,7 @@ $(document).ready(function(){
         else if(sorter == 2)
         {
             //week
+            RenderPieByWeek();
         }
         else if(sorter == 3)
         {
@@ -238,6 +239,66 @@ var RenderPieByYear = function()
         method : 'POST',
         data : {
             year : $("#year_selector").val()
+        },
+        dataType : "json",
+        beforeSend : function(){
+        },
+        success : function(data){
+            if(data.success)
+            {
+                var valid_pie = false;
+                $.each(data.content,function(key,value){
+                    var int_value = parseInt(value['value']);
+                    data.content[key]['value'] = int_value;
+                    if(int_value > 0)
+                    {
+                        valid_pie = true;
+                    }
+                });
+                
+                if(valid_pie)
+                {
+                    $(".no-data-found").css("display","none");
+                    $("#myPie").css("display","block");
+                    if(pie == null)
+                    {
+                        window.pie = new d3pie("myPie", {
+                                header: {
+                                },
+                                data: {
+                                        content: data.content
+                                }
+                        });
+                    }
+                    else
+                    {
+                        pie.updateProp("data.content", data.content);
+                    }
+                }
+                else
+                {
+                   $(".no-data-found").css("display","block");
+                   $("#myPie").css("display","none");
+                }
+            }
+            else
+            {
+                swal("Error", "Error connecting to server.", "error");
+            }
+        },
+        error : function(){
+            swal("Error", "Error connecting to server.", "error");
+        }
+    });
+};
+
+var RenderPieByWeek = function()
+{
+    $.ajax({
+        url : '/welcome/GetCrimeAnalysisByWeek',
+        method : 'POST',
+        data : {
+            week : $("#week_selector").val()
         },
         dataType : "json",
         beforeSend : function(){
