2bf5e7057b98d357b9f2f55b02b34c549e3fdd90
jethro <jethro@kayweb.com.au>
2017-08-28 21:27:22 +0800

    sql change on app_users tbl

--- sql/logs/8-28-17.sql
+++ sql/logs/8-28-17.sql
@@ -1,1 +1,3 @@
 ALTER TABLE `app_users` ADD `mobile` VARCHAR(15) NOT NULL AFTER `firstname`, ADD `address` VARCHAR(125) NOT NULL AFTER `mobile`;
+
+ALTER TABLE `app_users` ADD `is_citizen` TINYINT(1) NOT NULL DEFAULT '0' AFTER `is_admin`;
