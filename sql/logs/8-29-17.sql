a96dd28cde5500fea4d672252d5f28fa2aa0120e
jethro <jethro@kayweb.com.au>
2017-08-29 22:59:57 +0800

    is flag on crime reports

--- sql/logs/8-29-17.sql
+++ sql/logs/8-29-17.sql
@@ -1,3 +1,5 @@
 ALTER TABLE `crime_reports` ADD `user_id` INT NOT NULL AFTER `crime`;
 
 ALTER TABLE `crime_reports` ADD `date_reported` DATETIME NOT NULL AFTER `image`;
+
+ALTER TABLE `crime_reports` ADD `is_flag` TINYINT(1) NOT NULL AFTER `status`;
