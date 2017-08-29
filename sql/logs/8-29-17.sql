27ccbe4590d0dbd67b104bd3dd6bccb19527fd74
jethro <jethro@kayweb.com.au>
2017-08-29 23:30:49 +0800

    crime report comments

--- sql/logs/8-29-17.sql
+++ sql/logs/8-29-17.sql
@@ -3,3 +3,5 @@ ALTER TABLE `crime_reports` ADD `user_id` INT NOT NULL AFTER `crime`;
 ALTER TABLE `crime_reports` ADD `date_reported` DATETIME NOT NULL AFTER `image`;
 
 ALTER TABLE `crime_reports` ADD `is_flag` TINYINT(1) NOT NULL AFTER `status`;
+
+CREATE TABLE `icrime`.`crime_report_comments` ( `id` INT NOT NULL AUTO_INCREMENT , `crime_report_id` INT NOT NULL , `user_id` INT NOT NULL , `comment` TEXT NOT NULL , `date_added` DATETIME NOT NULL , PRIMARY KEY (`id`)) ENGINE = InnoDB;
