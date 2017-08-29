5b18053c5492afd57646e74965138b9f6771f959
jethro <jethro@kayweb.com.au>
2017-08-30 06:16:35 +0800

    comment list

---
+++ application/views/Admin/CrimeReports/CommentList.php
@@ -0,0 +1,11 @@
+<div class="col-xs-12" style="border-top: 1px solid #ccc;padding: 10px;">
+    <div class="pull-left">
+        <b><?php echo $user_name; ?></b>
+    </div>
+    <div class="pull-right">
+        <?php echo $date_added ; ?>
+    </div>
+    <div class="col-xs-12 no-gutter">
+        <?php echo $comment; ?>
+    </div>
+</div>
