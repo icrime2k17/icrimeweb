ae8e7dfbe5738c1da9f91ee8661878b5417d03db
jethro <jethro@kayweb.com.au>
2017-09-07 00:21:18 +0800

    crime analysis on app - done

--- application/controllers/Webservice.php
+++ application/controllers/Webservice.php
@@ -291,4 +291,80 @@ class Webservice extends CI_Controller {
         echo json_encode($json_data);
         exit;
     }
+    
+    public function GetBlotterYears()
+    {
+        $json_data = array();
+        $json_data['current_month'] = date('m');
+        $json_data['list'] = '<option value="">Select Year</option>';
+        $current_year = date('Y');
+        $years = $this->model->GetBlotterYears();
+        foreach ($years->result() as $y)
+        {
+            if($current_year == $y->years)
+            {
+                $json_data['list'] .= '<option value="'.$y->years.'" selected>'.$y->years.'</option>';
+            }
+            else
+            {
+                $json_data['list'] .= '<option value="'.$y->years.'">'.$y->years.'</option>';
+            }
+        }
+        $json_data['success'] = TRUE;
+        echo json_encode($json_data);
+        exit;
+    }
+    
+    public function GetCrimeAnalysisByMonth()
+    {
+        $date_from = date('Y-m-d',strtotime($_POST['year'].'-'.$_POST['month'].'-1'));
+        $date_to = date("Y-m-t", strtotime($date_from));
+        $json_data = array();
+        $content = $this->model->GetCrimeAnalysisByDateRange($date_from,$date_to);
+        $json_data['content'] = $content->result();
+        $json_data['success'] = TRUE;
+        echo json_encode($json_data);
+        exit;
+    }
+    
+    public function GetCrimeAnalysisByYear()
+    {
+        $date_from = date('Y-m-d',strtotime($_POST['year'].'-01-1'));
+        $date_to = date('Y-m-d',strtotime($_POST['year'].'-12-31'));
+        $json_data = array();
+        $content = $this->model->GetCrimeAnalysisByDateRange($date_from,$date_to);
+        $json_data['content'] = $content->result();
+        $json_data['success'] = TRUE;
+        echo json_encode($json_data);
+        exit;
+    }
+    
+    public function GetCrimeAnalysisByWeek()
+    {
+        $week_data = explode('-', $_POST['week']);
+        $year = $week_data[0];
+        $week = $week_data[1];
+        $week = ltrim($week, 'W');
+
+        $date_from = date('Y-m-d', strtotime($year."W".str_pad($week,2,"0",STR_PAD_LEFT)));
+        $date_to =  date('Y-m-d', strtotime("+6 days", strtotime($date_from)));
+        
+        $json_data = array();
+        $content = $this->model->GetCrimeAnalysisByDateRange($date_from,$date_to);
+        $json_data['content'] = $content->result();
+        $json_data['success'] = TRUE;
+        echo json_encode($json_data);
+        exit;
+    }
+    
+    public function GetCrimeAnalysisByDay()
+    {
+        $day = $_POST['day'];
+        $json_data = array();
+        $content = $this->model->GetCrimeAnalysisByDay($day);
+        $json_data['content'] = $content->result();
+        $json_data['success'] = TRUE;
+        echo json_encode($json_data);
+        exit;
+    }
 }
--- application/models/WebserviceModel.php
+++ application/models/WebserviceModel.php
@@ -300,6 +300,57 @@ Class WebserviceModel extends CI_Model {
             exit;
         }
     }
+    
+    public function GetBlotterYears() {
+        try
+        {
+            $sql = "SELECT DISTINCT(YEAR(date_of_incident)) as years 
+                    FROM blotter 
+                    WHERE enabled = 1 
+                    ORDER BY years DESC";
+            $stmt = $this->pdo->query($sql);
+            return $stmt;
+        } 
+        catch (Exception $ex) 
+        {
+            echo $ex;
+            exit;
+        }
+    }
+    
+    public function GetCrimeAnalysisByDateRange($date_from,$date_to) {
+        try
+        {
+            $sql = "SELECT c.crime as label,(SELECT COUNT(id) FROM blotter WHERE incident = c.crime
+                    AND (date_of_incident >= ? AND date_of_incident <= ?)) as value 
+                    FROM crimes as c
+                    ORDER BY c.crime";
+            $stmt = $this->pdo->query($sql,array($date_from,$date_to));
+            return $stmt;
+        } 
+        catch (Exception $ex) 
+        {
+            echo $ex;
+            exit;
+        }
+    }
+    
+    public function GetCrimeAnalysisByDay($day) {
+        try
+        {
+            $sql = "SELECT c.crime as label,(SELECT COUNT(id) FROM blotter WHERE incident = c.crime
+                    AND date_of_incident = ?) as value 
+                    FROM crimes as c
+                    ORDER BY c.crime";
+            $stmt = $this->pdo->query($sql,array($day));
+            return $stmt;
+        } 
+        catch (Exception $ex) 
+        {
+            echo $ex;
+            exit;
+        }
+    }
 }
 
 ?> 
