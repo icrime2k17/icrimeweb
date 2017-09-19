b72710a631154eff5244ba323a47b26845e11983
jethro <jethro@kayweb.com.au>
2017-09-20 01:18:10 +0800

    9-20-17

--- sql/logs/9-20-17.sql
+++ sql/logs/9-20-17.sql
@@ -1,1 +1,3 @@
 ALTER TABLE `crimes` ADD `type` TINYINT(1) NOT NULL DEFAULT '1' COMMENT '1 - major, 2 - minor' AFTER `crime`;
+
+ALTER TABLE `crimes` ADD `enabled` TINYINT(1) NOT NULL DEFAULT '1' AFTER `type`;
